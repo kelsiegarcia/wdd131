fbc9187c008a1df97fa0276f3aadff45032f5011
Kelsie Garcia <kelsie.c.garcia@gmail.com>
2024-09-27 16:57:45 -1000

    responsive nav menu added

--- week02/scripts/temples.js
+++ week02/scripts/temples.js
@@ -1,4 +1,10 @@
+// Store the selected elements that we are going to use. 
+const mainnav = document.querySelector('.navigation')
+const closeButton = document.querySelector('#menu');
 
-
-
+// Add a click event listender to the hamburger button and use a callback function that toggles the list element's list of classes.
+closeButton.addEventListener('click', () => {
+	mainnav.classList.toggle('show');
+	closeButton.classList.toggle('show');
+});
 
