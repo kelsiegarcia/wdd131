d37a41b0a409ae61b30610d4eeb20a4f449d8dcd
Kelsie Garcia <kelsie.c.garcia@gmail.com>
2024-10-05 09:44:07 -1000

    final

--- scripts/place.js
+++ scripts/place.js
@@ -8,11 +8,13 @@ year.innerHTML = `${today.getFullYear()}`;
 
 modified.innerHTML = `Last Modified: ${document.lastModified}`
 
-// Wind Chill Factor Function
-const temp = document.querySelector("#temp").innerHTML = `Temperature: 81\u00B0F`;
+const temp = 81;
+const wind = 13;
 const conditions = document.querySelector("#conditions").innerHTML = `Conditions: Mostly cloudy`;
-const wind = document.querySelector("#wind").innerHTML = `Wind: 13mph`;
 const humidity = document.querySelector("#humidity").innerHTML = `Humidity: 68%`;
+document.querySelector("#temp").innerHTML = `Temperature: ${temp}\u00B0F`;
+document.querySelector("#wind").innerHTML = `Wind: ${wind}mph`;
+
 
 function calculateWindChill(temp, wind) {
     return Math.round(35.74 + (0.6215 * temp) + (0.4275 * temp - 35.75) * Math.pow(wind, 0.16));
@@ -20,8 +22,7 @@ function calculateWindChill(temp, wind) {
 
 if (temp <= 50 && wind > 3) {
     const windChill = calculateWindChill(temp, wind);
-    console.log(`The wind chill factor for temp "${temp}" and wind speed "${wind}" is "${windChill}\u00B0F"`);
-		const windChillFactor = document.querySelector("#wind-chill").innerHTML = `Windchill:${windChill}\u00B0F`;
+		const windChillFactor = document.querySelector("#wind-chill").innerHTML = `Windchill: ${windChill}\u00B0F`;
 } else {
 		const windChill = "N/A";
     const windChillFactor = document.querySelector("#wind-chill").innerHTML = `Windchill: ${windChill}`;
