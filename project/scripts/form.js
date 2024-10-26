22daed316c2dd825187d7f13a2b01d6063e94aff
Kelsie Garcia <kelsie.c.garcia@gmail.com>
2024-10-26 09:12:57 -1000

    more form stylng

--- project/scripts/form.js
+++ project/scripts/form.js
@@ -39,17 +39,3 @@ form.addEventListener('submit', function(event) {
 	
 	window.location = 'confirmation.html';
 });
-
-
-const visitDisplay = document.getElementById('visits');
-let visits = localStorage.getItem('visits') || 0;
-
-if (visits === null) {
-	visits = 0;
-}	else {
-	visitDisplay.textContent = `You have visited this page ${visits} times.`;
-} 
-
-visits++; 
-
-localStorage.setItem('visits', visits); 
