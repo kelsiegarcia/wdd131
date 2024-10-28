e855637acd96d6d898774601cb3fc5b508d86182
Kelsie Garcia <kelsie.c.garcia@gmail.com>
2024-10-28 09:37:59 -1000

    more path changes and p textcontent updated

--- project/scripts/confirmation.js
+++ project/scripts/confirmation.js
@@ -21,7 +21,7 @@ if (data) {
 	p3.textContent = `Court and players: ${data.name}`;
 	p4.textContent = `Date and Time: ${formattedDate}.`;
 } else {
-	p.textContent = 'Court name is not available. Please check your booking details.';
+	p1.textContent = 'Court name is not available. Please check your booking details.';
 }
 
 if (display) {
